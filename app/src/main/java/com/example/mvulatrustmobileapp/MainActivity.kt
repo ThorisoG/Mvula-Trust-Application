9ab8caa57bcf766ee55085e5446af2b21706fa74
ThorisoG <127398329+ThorisoG@users.noreply.github.com>
2023-08-17 15:26:31 +0200

    Modified the splash screen to be automated ..tested it and it runs

--- app/src/main/java/com/example/mvulatrustmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mvulatrustmobileapp/MainActivity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 5000 // 5 seconds
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var home = findViewById<TextView>(R.id.text_link)
-        home.setOnClickListener {
-            var Intent = Intent(this, MainActivity2::class.java)
-            startActivity(Intent)
-        }
+        // Handler to navigate to the next activity after the splashTimeOut
+        Handler().postDelayed({
+            //the Main activity 2 is the intent where you want to go//
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
     }
 }
