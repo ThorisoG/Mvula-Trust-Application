2b91cb428cbac464ffe6c1b7661cdc0584c95a3f
GrizzlllllyT26 <kamogelomafuna2000@gmail.com>
2023-08-17 10:19:18 +0000

    Just added two empty Activity pages for the LoginPage & SiginUp_Page

---
+++ app/src/main/java/com/example/mvulatrustmobileapp/SiginUp_Page.kt
@@ -0,0 +1,11 @@
+package com.example.mvulatrustmobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiginUp_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigin_up_page)
+    }
+}
