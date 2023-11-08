6f7c936e268c25b5c5036421f6fb8ed1e7e3a5d6
ThorisoG <127398329+thorisog@users.noreply.github.com>
2023-11-08 14:44:04 +0200

    Added A feature where Administration can view Active volunteers on the database

--- app/src/main/java/com/example/mvulatrustmobileapp/activevolunteers.kt
+++ app/src/main/java/com/example/mvulatrustmobileapp/activevolunteers.kt
@@ -1,31 +1,36 @@
 package com.example.mvulatrustmobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
-import android.widget.SimpleCursorAdapter
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class activevolunteers : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activevolunteers)
 
-        val list = findViewById<ListView>(R.id.listview)
+        val activeVol = findViewById<TextView>(R.id.volunteerlist)
 
-        // Create an instance of DatabaseHelper
+        // Initialize your DatabaseHelper
         val dbHelper = DatabaseHelper(this)
 
-        // Get the list of approved volunteers
-        val cursor = dbHelper.getApprovedVolunteers()
+        // Get the list of approved volunteers from the database
+        val approvedVolunteers = dbHelper.getApprovedVolunteers()
 
-        // Define the columns from the cursor to use in the adapter
-        val fromColumns = arrayOf("Vname", "Program")
-        val toViews = intArrayOf(android.R.id.text1, android.R.id.text2)
+        if (approvedVolunteers.isEmpty()) {
+            // If no volunteers are found, set a specific message
+            activeVol.text = "No active volunteers at the moment"
+        } else {
+            // Construct a string to display in the TextView
+            val displayText = StringBuilder()
+            for (volunteer in approvedVolunteers) {
+                val name = volunteer[0]
+                val program = volunteer[1]
+                displayText.append("Name: $name,\n Program: $program\n")
+            }
 
-        // Create the adapter and set it to the ListView
-        val adapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, fromColumns, toViews, 0)
-        list.adapter = adapter
-
+            // Set the text in the TextView
+            activeVol.text = displayText.toString()
+        }
     }
 }
