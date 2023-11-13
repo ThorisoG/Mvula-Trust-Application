5c29843a1cb9f0c2069bb965e9d36cc58b6a88b4
Thoriso <thorisomatsitela@gmail.com>
2023-11-13 11:18:12 +0000

    update for lucias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
 
+    //
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -55,5 +56,6 @@ dependencies {
     //implementation of the chat-bot
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+
 
 }
